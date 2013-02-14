712f5f88399c65a3045ade2786ecc348adf611b5
Kazuhito Hokamura <k.hokamura@gmail.com>
2013-02-14 16:12:14 +0900

    Add test #2

--- test/tests/basic.js
+++ test/tests/basic.js
@@ -1,8 +1,11 @@
 describe('Flipsnap', function() {
-  var $sandbox = $('<div>').attr('id', 'sandbox').hide().appendTo('body');
-
-  afterEach(function() {
-    $sandbox.empty();
+  var f;
+  beforeEach(function() {
+    var $flipsnap = $('<div>')
+      .append('<div>')
+      .append('<div>')
+      .append('<div>');
+    f = Flipsnap($flipsnap.get(0));
   });
 
   describe('constructor', function() {
@@ -17,19 +20,111 @@ describe('Flipsnap', function() {
     context('when set string(css seclector)', function() {
       context('when exist element', function() {
         it('element should be search by selector', function() {
-          var $div1 = $('<div>').attr('id', 'div1').appendTo($sandbox);
-          var $div2 = $('<div>').attr('id', 'div2').appendTo($sandbox);
-          var f = Flipsnap('#sandbox div');
-          expect(f.element).to.be($div1.get(0));
+          var $foo = $('<div id="foo">').appendTo('body');
+          var f = Flipsnap('#foo');
+          expect(f.element).to.be($foo.get(0));
+          $foo.remove();
         });
       });
 
       context('when not exist element', function() {
         it('should throw error' ,function() {
           expect(function() {
-            Flipsnap('#sandbox div');
+            Flipsnap('#foo');
           }).to.throwError(/element not found/);
         });
+      });
+    });
+  });
+
+  describe('#hasNext', function() {
+    context('when has next element', function() {
+      it('should return true', function() {
+        expect(f.hasNext()).to.be(true);
+      });
+    });
+
+    context('when not has next element', function() {
+      it('should return false', function() {
+        f.moveToPoint(2);
+        expect(f.hasNext()).to.be(false);
+      });
+    });
+  });
+
+  describe('#hasPrev', function() {
+    context('when has prev element', function() {
+      it('should return false', function() {
+        f.moveToPoint(1);
+        expect(f.hasPrev()).to.be(true);
+      });
+    });
+
+    context('when not has prev element', function() {
+      it('should return true', function() {
+        expect(f.hasPrev()).to.be(false);
+      });
+    });
+  });
+
+  describe('#toNext', function() {
+    context('when currentPoint is not maxPoint', function() {
+      it('currentPoint should be increment', function() {
+        expect(f.currentPoint).to.be(0);
+        f.toNext();
+        expect(f.currentPoint).to.be(1);
+      });
+    });
+
+    context('when currentPoint is maxPoint', function() {
+      it('currentPoint should be not increment', function() {
+        f.moveToPoint(2);
+        expect(f.currentPoint).to.be(2);
+        f.toNext();
+        expect(f.currentPoint).to.be(2);
+      });
+    });
+  });
+
+  describe('#toPrev', function() {
+    context('when currentPoint is not 0', function() {
+      it('currentPoint should be increment', function() {
+        f.moveToPoint(1);
+        expect(f.currentPoint).to.be(1);
+        f.toPrev();
+        expect(f.currentPoint).to.be(0);
+      });
+    });
+
+    context('when currentPoint is 0', function() {
+      it('currentPoint should be not increment', function() {
+        expect(f.currentPoint).to.be(0);
+        f.toPrev();
+        expect(f.currentPoint).to.be(0);
+      });
+    });
+  });
+
+  describe('#moveToPoint', function() {
+    context('when argument greater than maxPoint', function() {
+      it('currentPoint should change to maxPoint', function() {
+        f.moveToPoint(5);
+        expect(f.currentPoint).to.be(2);
+      });
+    });
+
+    context('when argument less than 0', function() {
+      it('currentPoint should change to 0', function() {
+        f.moveToPoint(1);
+        f.moveToPoint(-1);
+        expect(f.currentPoint).to.be(0);
+      });
+    });
+
+    context('when argument betoween 0 and maxPoint', function() {
+      it('should change currentPoint', function() {
+        f.moveToPoint(1);
+        expect(f.currentPoint).to.be(1);
       });
     });
   });
